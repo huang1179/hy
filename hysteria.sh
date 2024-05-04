7ddd879059ba1b01066dde79e7b6215d58a37f70
huang1179 <88090243+huang1179@users.noreply.github.com>
2024-05-04 16:48:36 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -222,7 +222,7 @@ insthysteria(){
     fi
     ${PACKAGE_INSTALL} curl wget sudo qrencode procps iptables-persistent netfilter-persistent
 
-    wget -N https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/install_server.sh
+    wget -N https://raw.githubusercontent.com/huang1179/hy/main/install_server.sh
     bash install_server.sh
     rm -f install_server.sh
 
